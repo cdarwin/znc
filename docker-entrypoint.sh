4ed28be7fa58bda05f957a7fe29110adc02b9470
Jason Thigpen <darwin@senet.us>
2016-05-17 00:16:40 -0700

    Update entrypoint to clarify intent

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,6 +4,7 @@ set -e
 if [ "$1" = "znc" ]; then
   chown -R irc /var/run/ircd
   if [ "$#" -gt 1 ]; then
+    shift
     exec su-exec irc /usr/bin/znc "$@"
   else
     exec su-exec irc /usr/bin/znc --help
